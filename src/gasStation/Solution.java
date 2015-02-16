b0c74ce8b5781339d2d14ca6296b790154eab5f1
YungKC <kaicyung@gmail.com>
2015-02-16 13:15:08 -0800

    added a better solution

--- src/gasStation/Solution.java
+++ src/gasStation/Solution.java
@@ -15,18 +15,44 @@ Return the starting gas station's index if you can travel around the circuit onc
 
 Idea:
 loop through each station to get net, then order station ranked by net
-
 remove stations to check if net is < 0;
-
 have extra check if aggregate net is < 0. If so, then return -1
-
 trim number of stations by combining all consecutive negative ones to 1 station
+
+AnotherIdea:
+Use observation that the best starting station is when the last station has the worst net balance
 
  */
 public class Solution {
 
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-    	if (gas == null || cost == null || gas.length != cost.length)
+//	AnotherIdea:
+//	Use observation that the best starting station is when the last station has the worst net balance
+
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+    	if (gas == null || cost == null || gas.length != cost.length || gas.length == 0)
+    		return -1;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        for (int i=0; i<gas.length; i++){
+            sum += (gas[i] - cost[i]);
+            if (sum < min){
+                min = sum;
+                minIndex = i;
+            }
+        }
+
+        if (sum < 0)
+            return -1;
+
+        if (minIndex == gas.length - 1)
+            return 0;
+        else return minIndex + 1;
+    }
+    
+    public int canCompleteCircuitOriginal(int[] gas, int[] cost) {
+    	if (gas == null || cost == null || gas.length != cost.length || gas.length == 0)
     		return -1;
     	
         int numStations = gas.length;
@@ -78,6 +104,10 @@ public class Solution {
 		Solution sol = new Solution();
 		int[] gas;
 		int[] cost;
+		
+		gas = new int[]{9,5,5,5,5};
+		cost = new int[]{1,10,1,8,9};
+		System.out.println(sol.canCompleteCircuit(gas, cost) == 0);
 		
 		gas = new int[]{67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66};
 		cost = new int[]{27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
