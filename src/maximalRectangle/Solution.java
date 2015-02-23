c29f7986a4dcdb0a7e2de5bd6099a309fc0876b6
YungKC <kaicyung@gmail.com>
2015-02-23 15:53:22 -0800

    trying dp for maximal rectangle

--- src/maximalRectangle/Solution.java
+++ src/maximalRectangle/Solution.java
@@ -4,9 +4,26 @@ package maximalRectangle;
  * 
  */
 public class Solution {
-
     public int maximalRectangle(char[][] matrix) {
-
+    	int maxSize = 0;
+    	if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+    		return 0;
+    	
+    	// last index [0] == startRow# 1based, [1] == endRow# 1based
+    	int state[][][] = new int[matrix.length][matrix[0].length][2];		
+    	
+    	if (matrix[0][0] == '1') {
+    		state[0][0][0] = 1;
+    		state[0][0][1] = 1;
+    		maxSize = 1;
+    	}
+    	for (int r=0; r<matrix.length; r++) {
+    		for (int c=1; c<matrix[0].length; c++) {
+    			if (matrix[r][c] == '1') {
+    				state[r][c][0] == 
+    			}
+    		}
+    	}
     }
     
 	public static void main(String[] args) {
