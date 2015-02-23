ebe048bf8062c37a37f82baf440933f01ffbeb84
YungKC <kaicyung@gmail.com>
2015-02-23 14:51:15 -0800

    starting maximal rectangle

---
+++ src/maximalRectangle/Solution.java
@@ -0,0 +1,19 @@
+package maximalRectangle;
+/*
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+ * 
+ */
+public class Solution {
+
+    public int maximalRectangle(char[][] matrix) {
+
+    }
+    
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		
+		System.out.println(sol.maximalRectangle(new char[][]{{'1','0'}})==1);
+
+	}
+
+}
