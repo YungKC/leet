b7381ee05da0be08cdffbe15fc3c2a4bfb96b12d
YungKC <kaicyung@gmail.com>
2015-02-23 16:39:12 -0800

    in progress

--- src/maximalRectangle/Solution.java
+++ src/maximalRectangle/Solution.java
@@ -6,24 +6,37 @@ package maximalRectangle;
 public class Solution {
     public int maximalRectangle(char[][] matrix) {
     	int maxSize = 0;
-    	if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+    	int maxRow = matrix.length;
+    	if (matrix == null || maxRow == 0)
+    		return 0;
+    	int maxCol = matrix[0].length; 
+    	if (matrix[0].length == 0)
     		return 0;
     	
     	// last index [0] == startRow# 1based, [1] == endRow# 1based
-    	int state[][][] = new int[matrix.length][matrix[0].length][2];		
+    	int state[][][] = new int[maxRow][maxCol][2];		
     	
     	if (matrix[0][0] == '1') {
     		state[0][0][0] = 1;
     		state[0][0][1] = 1;
     		maxSize = 1;
     	}
-    	for (int r=0; r<matrix.length; r++) {
-    		for (int c=1; c<matrix[0].length; c++) {
-    			if (matrix[r][c] == '1') {
-    				state[r][c][0] == 
-    			}
+    	for (int r=1; r<matrix.length; r++) {
+    		if (matrix[r][0] == '1') {
+    			state[r][0][0] = state[r-1][0][0];
+    			state[r][0][1] = state[r-1][0][1];
+    			
     		}
     	}
+
+    	for (int c=1; c<maxCol; c++) {
+    		if (matrix[0][c] == '1') {
+    			state[0][c][0] = state[0][c-1][0];
+    			state[0][c][1] = state[0][c-1][1];
+    		}
+    	}
+
+    	
     }
     
 	public static void main(String[] args) {
