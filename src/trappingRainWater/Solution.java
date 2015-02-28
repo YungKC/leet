e908f229b72e0bb30edf18ae7a2a1b782415a228
YungKC <YungKC@gmail.com>
2015-02-27 21:34:13 -0800

    finally solved the trapping rain water problem.

--- src/trappingRainWater/Solution.java
+++ src/trappingRainWater/Solution.java
@@ -15,110 +15,83 @@ The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In th
 
 idea:
 
-find first left index where height starts dropping
-find last right index where height stops rising
+The the first left peak
+for each subsequent peak, 
+	if current peak is higher than last peak, 
+		then add up the collected rain from this peak leftward, and reset tallest peaks
 
-use a stack to push last peak index. Height can be retrieved from A[index]
+At the end, the water level is same as the right peak, so add up rain water from last tallest peak to this right peak.
 
-now, traverse from leftmost peak, toward right
-
-starts from peak at x1
-at peak x2: 
-
-if h(x2) <= h(x1), then add up rain from x1 to x2
-else 
-	identify the next peak toward left until at x0 so that h(x0) > h(x2)
-	increase total rain by (x2-x0+1)*h(x2)-h(x1)
-		(special case: if x0 is beyond the xLeft limit, then no need to increment this)
  */
 public class Solution {
     public int trap(int[] A) {
-    	
     	
     	if (A == null || A.length <= 2)
     		return 0;
     	
     	
-        int curLeft = 1;
-        while (curLeft < A.length && A[curLeft] >= A[curLeft-1])
-        	curLeft++;
-        int curRight = A.length-1;
-        while (curRight > 0 && A[curRight] <= A[curRight-1])
-        	curRight--;
+        int headPeakXPos = 1;
+        while (headPeakXPos < A.length && A[headPeakXPos] >= A[headPeakXPos-1])
+        	headPeakXPos++;
+        int tailPeakXPos = A.length-1;
+        while (tailPeakXPos > 0 && A[tailPeakXPos] <= A[tailPeakXPos-1])
+        	tailPeakXPos--;
         
-        if (curLeft >= curRight)
+        if (headPeakXPos >= tailPeakXPos)
         	return 0;
         
-        curLeft--;
+        headPeakXPos--;
         
         int totalRain = 0;
         
-        // populate the stack with location of each peaks
-    	List<Integer> heightStack = new ArrayList<Integer>();    
-    	
-    	heightStack.add(curLeft);
-    	   		 
+    	int tallestPeakXPos = headPeakXPos;
+    	int tallestPeakHeight = A[headPeakXPos];
+    	int curPeakXPos = headPeakXPos+1;
     	do {
-    		while (++curLeft < curRight && A[curLeft] < A[curLeft-1]);
-    		while (++curLeft < curRight && A[curLeft] > A[curLeft-1]);
-    		if (curLeft < curRight) {
-    	    	heightStack.add(curLeft-1);
+    		// find next peak at curPeakXPos
+    		while (curPeakXPos < tailPeakXPos && A[curPeakXPos] <= A[curPeakXPos-1]) {
+    			curPeakXPos++;
     		}
-    	} while (curLeft < curRight);
-    	
-    	heightStack.add(curRight);
-        
-//    	starts from peak at x1
-//    	at peak x2: 
-//
-//    	add up rain from x1 to x2
-//    	if h(x2) > h(x1) 
-//    		identify the next peak toward left until at x0 so that h(x0) > h(x2)
-//    		increase total rain by (x2-x0+1)*h(x2)-h(x1)
-//    			(special case: if x0 is beyond the xLeft limit, then no need to increment this)
-//				(special case: if peak spans more than 1 width)    			
-    	int lastPeakXPos = heightStack.get(0);
-    	int edgeHeight = A[lastPeakXPos];
-    	int lastHigherPeakListIndex = 0;
-    	int curListIndex = 1;
-    	while (curListIndex < heightStack.size()) {
-    		int curXPos = heightStack.get(curListIndex);
-    		int curHeight = A[curXPos];
-
-    		if (curHeight < edgeHeight) {
-    			for (int x = lastPeakXPos+1; x<curXPos; x++) {
-    				if (curHeight - A[x] > 0)
-    					totalRain += curHeight - A[x];
-    			}
+    		while (curPeakXPos <= tailPeakXPos && A[curPeakXPos] > A[curPeakXPos-1]) {
+    			curPeakXPos++;
     		}
-    		else {
-    			for (int x = lastPeakXPos+1; x<curXPos; x++) {
-    				if (edgeHeight - A[x] > 0)
-    					totalRain += edgeHeight - A[x];
+    		curPeakXPos--;
+    		// if current peak is >= first peak, then we can sum up the rain and drop all previous peaks
+    		if (A[curPeakXPos] >= tallestPeakHeight) {
+    			for (int x=tallestPeakXPos+1; x<curPeakXPos; x++) {
+    				if (tallestPeakHeight > A[x])
+    					totalRain += tallestPeakHeight - A[x];
     			}
-    			// find peak toward left that's >= curHeight
-    			do {
-    				--lastHigherPeakListIndex;
-    			} while (lastHigherPeakListIndex >= 0 && A[heightStack.get(lastHigherPeakListIndex)] < curHeight);
-    			if (lastHigherPeakListIndex >= 0) {
-    				totalRain += (curXPos - heightStack.get(lastHigherPeakListIndex) - 1) * (curHeight - edgeHeight);
-    			}
-    				
-    			lastHigherPeakListIndex = curListIndex;
-    			lastPeakXPos = heightStack.get(lastHigherPeakListIndex);
-        		edgeHeight = curHeight;
+    			tallestPeakXPos = curPeakXPos;
+    			tallestPeakHeight = A[curPeakXPos];
     		}
-    		curListIndex++;
-    	}
+			curPeakXPos++;
+    	} while (curPeakXPos < tailPeakXPos);
+    		
+    	// update rain volume from peaks of decreasing heights
+    	int waterHeight = A[tailPeakXPos];
+    	for (int x=tailPeakXPos-1; x>tallestPeakXPos; x--) {
+			if (waterHeight > A[x])
+				totalRain += waterHeight - A[x];
+			else
+				waterHeight = A[x];
+		}
 
     	return totalRain;
     }
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		
-		System.out.println(sol.trap(new int[]{1,7,8}) == 0);
+		System.out.println(sol.trap(new int[]{9,6,8,8,5,6,3}) == 3);
+		System.out.println(sol.trap(new int[]{4,3,3,9,3,0,9,2,8,3}) == 23);
+		System.out.println(sol.trap(new int[]{5,4,1,2}) == 1);
+		System.out.println(sol.trap(new int[]{6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3}) == 83);
+		System.out.println(sol.trap(new int[]{5,5,1,7,1,1,5,2,7,6}) == 23);
+		System.out.println(sol.trap(new int[]{4,2,0,3,2,5}) == 9);
+		System.out.println(sol.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}) == 6);
 		System.out.println(sol.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}) == 6);
 		System.out.println(sol.trap(new int[]{1,0,1}) == 1);
+		System.out.println(sol.trap(new int[]{1,7,8}) == 0);
 
 	}
 
