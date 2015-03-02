bbd688405aaa4fe4e42af08c714bd752f3632b8e
YungKC <YungKC@gmail.com>
2015-03-01 16:00:08 -0800

    almost, but 1 more test case to pass

--- src/medianTwoSortedArrays/Solution.java
+++ src/medianTwoSortedArrays/Solution.java
@@ -38,9 +38,15 @@ public class Solution {
     		shorter = A;
     	}
     	
+
+	    return inner(shorter, longer);
+    }
+
+    private double inner(int[] shorter, int[] longer) {
      	int longerLeft=0, shorterLeft=0, longerRight=longer.length-1, shorterRight=shorter.length-1;
    		int totalLength = longer.length + shorter.length;
-   		int idealMedianIndex = totalLength/2;
+   		boolean isTotalLengthOdd = totalLength%2 == 1;
+   		int idealNumLeftElements = totalLength/2;
 
      	// [1 2], [4 5]
     	if (shorter[shorterRight] <= longer[longerLeft]) {
@@ -49,7 +55,7 @@ public class Solution {
     		}
     		else {
     			int centerIndex = (longer.length-shorter.length)/2;
-    			if (totalLength%2 == 0) {
+    			if (!isTotalLengthOdd) {
     				return (longer[centerIndex-1] + longer[centerIndex])/2.0;
     			}
     			else {
@@ -64,7 +70,7 @@ public class Solution {
     		}
     		else {
     			int centerIndex = shorter.length + (longer.length-shorter.length)/2;
-    			if (totalLength%2 == 0) {
+    			if (!isTotalLengthOdd) {
     				return (longer[centerIndex-1] + longer[centerIndex])/2.0;
     			}
     			else {
@@ -77,150 +83,93 @@ public class Solution {
     	// compare the num elements on either side of this value
     	// adjust left or right boundary of longer array, and repeat
 
+// 		int shortStartIndex = 0;
+    	int medianShorterIndex  = 0;
+// 		int shortEndIndex = shorter.length-1;
+    	boolean isFailed = false;
      	do {
-     		int shortStartIndex = 0;
-     		int shortEndIndex = shorter.length-1;
    			int medianLongerIndex = (longerLeft + longerRight)/2;
 	    	int medianValue = longer[medianLongerIndex];
-	    	int medianShorterIndex = findMedianIndex(shorter, shortStartIndex, shortEndIndex, medianValue);
-	    	if (medianShorterIndex >= shortStartIndex) {
-	    		shortStartIndex = medianShorterIndex;
+	    	medianShorterIndex= findMedianIndex(shorter, 0, shorter.length-1, medianValue);
+
+	    	int numLeftElements = medianShorterIndex + medianLongerIndex;
+	    	if (shorter[medianShorterIndex] <= medianValue)
+	    		numLeftElements++;
+	    	
+	    	int numElementsMoreOnLeft = numLeftElements - idealNumLeftElements;
+
+	    	if (numElementsMoreOnLeft == 0 || (!isTotalLengthOdd && numElementsMoreOnLeft == -1)) {
+		    	// now we are narrowed to picking the median
+		    	if (medianLongerIndex == 0) {
+		    		if (isTotalLengthOdd)
+		    			return longer[0];
+		    		else
+		    			return (longer[0] + shorter[shorter.length-1])/2.0;
+		    	}
+		    	else if (medianLongerIndex == longer.length - 1) {
+		    		if (isTotalLengthOdd) {
+		    			return longer[longer.length-1];
+		    		}
+		    		else
+		    			return (longer[longer.length-1] + shorter[0])/2.0;
+		    	}
+		    	else if (longer[medianLongerIndex-1] > shorter[shorter.length-1] || longer[medianLongerIndex+1] < shorter[0]) {
+		    		if (isTotalLengthOdd)
+		    			return longer[medianLongerIndex];
+		    		else
+		    			return (longer[medianLongerIndex] + longer[medianLongerIndex+1])/2.0;
+		    	}
+		    	else {
+		    		if (isTotalLengthOdd)
+		    			return longer[medianLongerIndex];
+		    		else {
+		    			if (numElementsMoreOnLeft >= 0) {
+		    				int nextValue = longer[medianLongerIndex-1];
+		    				if (longer[medianLongerIndex] > shorter[medianShorterIndex] && shorter[medianShorterIndex] > nextValue)
+		    					nextValue = shorter[medianShorterIndex];
+			    			return (longer[medianLongerIndex] + nextValue)/2.0;
+		    			}
+		    			else {
+		    				int nextValue = longer[medianLongerIndex+1];
+		    				if (longer[medianLongerIndex] < shorter[medianShorterIndex] && shorter[medianShorterIndex] < nextValue)
+		    					nextValue = shorter[medianShorterIndex];
+			    			return (longer[medianLongerIndex] + nextValue)/2.0;
+		    			}
+		    		}
+		    	}
+	    	}
+	    	else if (numElementsMoreOnLeft > 0) {
+	    		if (longerRight == medianLongerIndex)
+	    			isFailed = true;
+	    		else
+	    			longerRight = medianLongerIndex;
 	    	}
 	    	else {
-	    		shortEndIndex = medianShorterIndex;
-	    	}
-	    	int leftElements = medianShorterIndex + medianLongerIndex;
-	    	int diff = leftElements - idealMedianIndex;
-	    	if (diff > 1) {
-	    		longerRight = medianLongerIndex;
-	    	}
-	    	else if (diff < 1) {
-	    		longerLeft = medianLongerIndex;
-	    	}
-	    	// now we are narrowed to picking the median
-	    	if (medianLongerIndex == 0) {
-	    		if (totalLength%2 == 1)
-	    			return longer[0];
+	    		if (longerLeft == medianLongerIndex)
+	    			isFailed = true;
 	    		else
-	    			return (longer[0] + shorter[shorter.length-1])/2.0;
+	    			longerLeft = medianLongerIndex;
 	    	}
-	    	else if (medianLongerIndex == longer.length - 1) {
-	    		if (totalLength%2 == 1) {
-	    			return longer[longer.length-1];
-	    		}
-	    		else
-	    			return (longer[longer.length-1] + shorter[0])/2.0;
-	    	}
-	    	else if (medianLongerIndex == 0 || medianLongerIndex >= longer.length -1 || longer[medianLongerIndex-1] > shorter[shorter.length] || longer[medianLongerIndex+1] < shorter[0]) {
-	    		if (longer.length % 2 == 1)
-	    			return longer[medianLongerIndex];
-	    		else
-	    			return (longer[medianLongerIndex] + longer[medianLongerIndex+1])/2.0;
-	    	}
-		} while (longerRight - longerLeft > 1);
-
-	    	
-
-
-
-
-
-
-/*
-    	// special case where short array has only 1 element
-    	if (shorter.length == 1) {
-        	int midPt = longer.length/2;
-        	if (longer.length % 2 == 1) {
-        		if (shorter[0] >= longer[midPt-1] && shorter[0] <= longer[midPt+1])
-        			return (shorter[0] + longer[midPt])/2.0;
-        		else if (shorter[0] < longer[midPt-1])
-        			return (longer[midPt-1] + longer[midPt])/2.0;
-        		else
-        			return (longer[midPt+1] + longer[midPt])/2.0;
-        	}
-        	else {
-        		if (shorter[0] >= longer[midPt-1] && shorter[0] <= longer[midPt])
-        			return shorter[0];
-        		else if (shorter[0] < longer[midPt-1])
-        			return longer[midPt-1];
-        		else
-        			return longer[midPt];
-        	}
-    	}
+		} while (!isFailed);
     	
-    	
-    	int longerLeft=0, shorterLeft=0, longerRight=longer.length-1, shorterRight=shorter.length-1;
-
-		int longerMidX2 = longerRight+longerLeft;
-		int shorterMidX2 = shorterRight+shorterLeft;
-		
-    	while (shorterRight-shorterLeft >= 2) {
-    		longerMidX2 = longerRight+longerLeft;
-    		shorterMidX2 = shorterRight+shorterLeft;
-    		int longerMidValue = longer[longerMidX2/2];
-    		int shorterMidValue = shorter[shorterMidX2/2];
-    		
-    		if (longerMidValue > shorterMidValue) {
-    			int tmp = shorterLeft;
-				shorterLeft = shorterMidX2/2;
-				longerRight = longerRight - shorterLeft + tmp;
-    		}
-    		else {
-    			int tmp = shorterRight;
-				shorterRight = shorterMidX2/2;
-				longerLeft = longerLeft - shorterRight + tmp;
-    		}
-    	}
-    	// we are now left with shorter array is bound by 2 values
-    	// 5 possible cases:
-    	int centerLeft = (longerLeft + longerRight)/2;
-    	int centerRight = (longerLeft + longerRight) % 2 == 0 ? centerLeft : centerLeft+1;
-    	
-    	// [1 2], [4 5]
-    	if (shorter[shorterRight] <= longer[centerLeft]) {
-    		if (centerLeft == 0) {
-    			return (shorter[shorterRight] + longer[0])/2.0;
-    		}
-    		else {
-    			return (longer[centerLeft-1] + longer[centerRight-1])/2.0;
-    		}
-    	}
-    	// [4 5], [1 2]
-    	else if (shorter[shorterLeft] >= longer[centerRight]) {
-    		if (centerRight == longer.length-1) {
-    			return (shorter[shorterLeft] + longer[centerRight])/2.0;
-    		}
-    		else {
-    			return (longer[centerLeft+1] + longer[centerRight+1])/2.0;
-    		}
-    	}
-    	// [4 5], [2 7]
-    	else if (shorter[shorterLeft] >= longer[centerLeft] && shorter[shorterRight] <= longer[centerRight]) {
-    		return (shorter[shorterLeft] + shorter[shorterRight])/2.0;
-    	}
-    	// [1 3], [2 4]
-    	else if (shorter[shorterLeft] <= longer[centerLeft] && shorter[shorterRight] <= longer[centerRight]) {
-    		return (shorter[shorterRight] + longer[centerLeft])/2.0;
-    	}
-    	// [2 4], [1 3]
-    	else {
-    		return (longer[centerRight] + shorter[shorterLeft])/2.0;
-    	}
-
-*/
-
-    	return -1;
+     	return inner(longer, shorter);
     }
-
+    
+    
+    
     private int findMedianIndex(int[] array, int left, int right, int medianValue) {
-    	int median = (left+right)/2;
-    	while (right-left > 1) {
+    	if (medianValue >= array[right])
+    		return right;
+    	else if (medianValue <= array[left])
+    		return left;
+    	while (right - left > 1) {
+        	int median = (left+right)/2;
     		if (array[median] > medianValue)
-    			left = median;
-    		else if (array[median] < medianValue) 
     			right = median;
+    		else if (array[median] < medianValue) 
+    			left = median;
     	}
-    	return left;
+    	return right;
     }
     	
     
@@ -235,17 +184,17 @@ public class Solution {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		
-		System.out.println(sol.findMedianSortedArrays(new int[]{1,2,4}, new int[]{3,5,6}) == 3.5);
-		System.out.println(sol.findMedianSortedArrays(new int[]{1,2,4}, new int[]{3,5,6,7,8}) == 4);
-		System.out.println(sol.findMedianSortedArrays(new int[]{1,2,3}, new int[]{1,4,5,6,7,8}) == 4);
+
+		System.out.println(sol.findMedianSortedArrays(new int[]{1,4,5,6}, new int[]{2,3,7,8}) == 4.5);		
 		System.out.println(sol.findMedianSortedArrays(new int[]{1,2,3}, new int[]{1,4,5,6,7,8,9}) == 4.5);
+
+		System.out.println(sol.findMedianSortedArrays(new int[]{1,2,4}, new int[]{3,5,6,7,8}) == 4.5);
+		System.out.println(sol.findMedianSortedArrays(new int[]{1,2,4}, new int[]{3,5,6}) == 3.5);
+
+		System.out.println(sol.findMedianSortedArrays(new int[]{1,2,3}, new int[]{1,4,5,6,7,8}) == 4);
 		System.out.println(sol.findMedianSortedArrays(new int[]{8,9,10}, new int[]{1,4,5,6,7,8}) == 7);
 		System.out.println(sol.findMedianSortedArrays(new int[]{8,9,10}, new int[]{1,4,5,6,7,8,9}) == 7.5);
 
-
-
-		System.out.println(sol.findMedianSortedArrays(new int[]{1,4,5,6}, new int[]{2,3,7,8}) == 4.5);		
-		System.out.println(sol.findMedianSortedArrays(new int[]{1,1,3,3}, new int[]{1,1,3,3}) == 2);
 		System.out.println(sol.findMedianSortedArrays(new int[]{2,10}, new int[]{1,2,3,4,5,6,7,8,9,10}) == 5.5d);
 
 
@@ -263,6 +212,7 @@ public class Solution {
 		System.out.println(sol.findMedianSortedArrays(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1}, new int[]{0}) == 1d);
 		System.out.println(sol.findMedianSortedArrays(new int[]{}, new int[]{1,2,3,4}) == 2.5d);
 
+		System.out.println(sol.findMedianSortedArrays(new int[]{1,1,3,3}, new int[]{1,1,3,3}) == 2);
 
 
 	}
