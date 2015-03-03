3481bdd902577fb778af8e0238284d31b30d62b0
YungKC <YungKC@gmail.com>
2015-03-02 21:51:12 -0800

    starting best time to sell stocks

---
+++ src/bestTimeToSellStocks/Solution.java
@@ -0,0 +1,29 @@
+package bestTimeToSellStocks;
+/*
+ * 
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+ */
+public class Solution {
+
+    public int maxProfit(int k, int[] prices) {
+    	
+    	
+    	
+    	
+//        return 20;
+    }
+    
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		
+		System.out.println(sol.maxProfit(10, new int[]{10,30}) == 20);
+
+	}
+
+}
