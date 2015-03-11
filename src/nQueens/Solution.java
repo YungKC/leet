466b469c061b75b9715d6e761d7806768c6a5c5e
YungKC <kaicyung@gmail.com>
2015-03-11 15:58:26 -0700

    finished bestTimeToSellStocksIII, and starting nQueens

---
+++ src/nQueens/Solution.java
@@ -0,0 +1,38 @@
+package nQueens;
+/*
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+For example,
+There exist two distinct solutions to the 4-queens puzzle:
+
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+
+ */
+public class Solution {
+
+    public List<String[]> solveNQueens(int n) {
+        
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
